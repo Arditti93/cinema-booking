f93817ec53e16758186658efd7ea0011532c7a8b
Alex Arditti <alex.arditti@hotmail.com>
2020-08-18 12:07:40 +0100

    #4 Pricing structure
    added prices as variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+
+//screenNumberofseats
 let twoD = 200
 let threeD = 150
 let both = 300
 
+
+//ticket prices
+let child = 3 
+let adult = 10
+let senior = 5
 
 class order {
     constructor( name, age, popcorn, hotDog, softDrink) {
@@ -15,13 +22,13 @@ class order {
 
     price() {
         if (this.age <= 7) {
-            return("Your child ticket price will be £3")
+            return(`Your child ticket price will be, ${child}`)
         }
         else if (this.age >= 7 && this.age <= 65) {
-                return("Your adult ticket price will be £10")
+                return(`Your adult ticket price will be, ${adult}`)
         }
         else if (this.age >= 65) { 
-            return("Your senior ticket price will be £5")
+            return(`Your senior ticket price will be, ${senior}`)
 
         }
         else {
