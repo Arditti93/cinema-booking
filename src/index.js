fbb48add70c6c08476d60a873951eaa7a08819de
Alex Arditti <alex.arditti@hotmail.com>
2020-08-18 15:40:32 +0100

    #16 create student price
    enhanced price function to  add student

--- src/index.js
+++ src/index.js
@@ -4,21 +4,25 @@ let both = 300
 
 
 class order {
-    constructor(name, age, food, drink) {
+    constructor(name, age, student, food, drink) {
         this.name = name; 
         this.age = age; 
+        this.student = student
         this.food = food;
         this.drink = drink;
     } 
 
     price() {
-        if (this.age <= 7) {
+        if(this.student == "no" && this.age <= 7) {
             return("Your child ticket price will be £3")
         }
-        else if (this.age >= 7 && this.age <= 65) {
+        else if (this.student == "no" && this.age >= 7 && this.age <= 65) {
                 return("Your adult ticket price will be £10")
         }
-        else if (this.age >= 65) { 
+        else if(this.student == "yes" && this.age >= 7 && this.age <=65) {
+            return("Your student price will be £6")
+        }
+        else if (this.student == "no" && this.age >= 65) { 
             return("Your senior ticket price will be £5")
 
         }
@@ -26,6 +30,7 @@ class order {
             return("Invaild age")
         };
     }
+
     refreshmentsFood() {
         if(this.food === "Twix") {
             console.log(`Your choice is: ${this.food}`)
@@ -55,3 +60,11 @@ class order {
         }
     }
 }
+
+//* TEST CODE
+
+const newPerson = new order ("James", 24, "yes", "Twix","Water")
+
+console.log(newPerson.price())
+// console.log(newPerson.refreshmentsFood())
+// console.log(newPerson.refreshmentsDrink())
